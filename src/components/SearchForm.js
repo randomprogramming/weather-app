ee8e0a44a1d483d024d25e1a474682046b0268b5
randomprogramming <isnipedodac@gmail.com>
2019-11-23 19:09:49 +0100

    added the search button and field, made them somewhat pretty

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import "./css/SearchForm.css";
+
 export default class SearchForm extends Component {
+	updateCity = () => {
+		console.log("Works!");
+	};
+
 	render() {
 		return (
 			<div>
-				<input type="text" placeholder="Search for your city..."></input>
-				<span>
-					<FontAwesomeIcon icon={faSearch} color="blue" />
+				<input className="search-field" type="text" placeholder="Search for your city..."></input>
+				<span className="search-button" onClick={this.updateCity.bind(this)}>
+					<FontAwesomeIcon icon={faSearch} color="white" size="2x" />
 				</span>
 			</div>
 		);
