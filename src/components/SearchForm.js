b99aee320d8435ba226cfb5b6b902cdb5b3eacae
randomprogramming <isnipedodac@gmail.com>
2019-11-23 19:18:59 +0100

    added search bar functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,15 +5,32 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./css/SearchForm.css";
 
 export default class SearchForm extends Component {
-	updateCity = () => {
-		console.log("Works!");
+	//TODO: Add a small box under the search bar which will show all cities from search results
+	constructor() {
+		super();
+		this.state = {
+			searchFieldValue: ""
+		};
+	}
+	searchFieldOnUpdate = event => {
+		this.setState({
+			searchFieldValue: event.target.value
+		});
+	};
+	searchCities = () => {
+		console.log(this.state.searchFieldValue);
 	};
 
 	render() {
 		return (
 			<div>
-				<input className="search-field" type="text" placeholder="Search for your city..."></input>
-				<span className="search-button" onClick={this.updateCity.bind(this)}>
+				<input
+					className="search-field"
+					onChange={this.searchFieldOnUpdate.bind(this)}
+					type="text"
+					placeholder="Search for your city..."
+				></input>
+				<span className="search-button" onClick={this.searchCities.bind(this)}>
 					<FontAwesomeIcon icon={faSearch} color="white" size="2x" />
 				</span>
 			</div>
