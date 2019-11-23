fa428a1a5464e78132a47559bd962667cff61121
randomprogramming <isnipedodac@gmail.com>
2019-11-23 22:18:10 +0100

    added some comments

--- src/components/CityDiv.js
+++ src/components/CityDiv.js
@@ -8,6 +8,8 @@ export default class CityDiv extends Component {
 		const { cityName, country, countryCode } = this.props;
 		return (
 			<div>
+				{/* When the user clicks on one of the CityDivs, get the url for the city that they clicked and redirect them to that url
+        This will reload the state of WeatherInfo and all data will load as it should //TODO: Make this pretty */}
 				<a href={this.getRedirectUrl()}>
 					{cityName} {country} {countryCode}
 				</a>
