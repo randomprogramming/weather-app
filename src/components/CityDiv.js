31d8b4962710b5761d89a474f20db2333d208cd5
randomprogramming <isnipedodac@gmail.com>
2019-11-29 19:09:21 +0100

    removed <a> tag with a clickable div in CityDiv

--- src/components/CityDiv.js
+++ src/components/CityDiv.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 
 export default class CityDiv extends Component {
 	getRedirectUrl = () => {
-		return "/" + this.props.countryCode + "/" + this.props.cityName;
+		//window.location =  / " + {this.props.countryCode} + " / " + this.props.cityName;;
+		window.location = "/" + this.props.countryCode + "/" + this.props.cityName;
 	};
 	render() {
 		const { cityName, country, countryCode } = this.props;
 		return (
-			<div className="city-div">
+			<div className="city-div" onClick={this.getRedirectUrl}>
 				{/* When the user clicks on one of the CityDivs, get the url for the city 
         that they clicked and redirect them to that url.
-        This will reload the state of WeatherInfo and all data will load as it should 
-        //TODO: Make this pretty */}
-				<a className="city-a" href={this.getRedirectUrl()}>
+        This will reload the state of WeatherInfo and all data will load as it should */}
+				<span className="city-span">
 					{cityName}, {country}, {countryCode}
-				</a>
+				</span>
 			</div>
 		);
 	}
